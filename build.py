a07c5caa2fdcba2f36504b27849529800b486b4f
nixen420 <nikhil.singh4008@gmail.com>
2017-06-20 06:22:09 +0000

    Done

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 import pandas as pd
-
-
 def solution(array):
-    """
-    Enter your code here
-    """
+        #array=[2, 4, 6, 8, 10]
+        panda_series=pd.Series(array)
+        return panda_series
+        #return panda_series.index
+#solution(array)
